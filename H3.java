0100cb8d64696759816ca1aaf5bf0c5cbfa9dd5d
Vladyslav <133018052+Vinum96@users.noreply.github.com>
2023-07-03 10:23:35 +0200

    Add files via upload

---
+++ H3.java
@@ -0,0 +1,13 @@
+public class H3 {
+    public static void main(String[] args) {
+        String str = new String("I study Basic Java!");
+        runStringMethod(String. str) {
+        System.out.println(str.charAt(str.length() - 2));
+        System.out.println(str.contains("Java"));
+        System.out.println(str.substring(str.length() - 5, str.length() - 1));
+        System.out.println(str.replace('a', 'o'));
+        System.out.println(str.toUpperCase(Locale.ROOT));
+        System.out.println(str.toLowerCase(Locale.ROOT));
+    }
+}
+
